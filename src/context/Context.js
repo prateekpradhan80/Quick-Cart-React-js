6baf4a56874bb7f59af7395f7e4ea3baf5f9d20d
Prateek <prateekpradhan80@gmail.com>
2024-02-13 10:47:06 +0530

    done

---
+++ src/context/Context.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useReducer } from "react";
+import { faker } from "@faker-js/faker";
+import { cartReducer, productReducer } from "./Reducers";
+
+const Cart = createContext();
+faker.seed(24);
+
+const Context = ({ children }) => {
+  const products = [...Array(21)].map(() => ({
+    id: faker.string.uuid(),
+    name: faker.commerce.productName({ category: "cloth" }),
+    price: faker.commerce.price(),
+    image: faker.image.urlLoremFlickr({ category: "dress" }),
+    inStock: faker.number.int({ min: 0, max: 4 }),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.number.int({ min: 1, max: 5 }),
+  }));
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+
+  const [productState, productDispatch] = useReducer(productReducer, {
+    byStock: false,
+    byFastDelivery: false,
+    byRating: 0,
+    searchQuery: "",
+  });
+
+  return (
+    <Cart.Provider value={{ state, dispatch, productState, productDispatch }}>
+      {" "}
+      {children}{" "}
+    </Cart.Provider>
+  );
+};
+
+export default Context;
+
+export const CartState = () => {
+  return useContext(Cart);
+};
